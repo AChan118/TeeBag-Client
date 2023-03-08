170d12b9df795b87c52fe238edfb0312151654df
Adam Chance <adamchance118@gmail.com>
2023-03-08 16:04:43 -0600

    Changed app name in html

--- src/components/profile/EditProfile.js
+++ src/components/profile/EditProfile.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getCurrentUser, updateUser } from "../../managers/UserManager";
+import { Button } from "flowbite-react";
 
 export const UpdateProfile = () => {
     const [currentUser, setCurrentUser] = useState({});
@@ -69,7 +70,7 @@ export const UpdateProfile = () => {
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Bio</label>
                         <textarea onChange={changeProfileState} id="bio" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " value={currentProfile.bio}></textarea>
                     </div>
-                    <button onClick={handleUpdateButton} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    <Button onClick={handleUpdateButton} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</Button>
                 </form>
             </div>
         </div>
